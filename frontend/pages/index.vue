a95d2c23175c7c4ea7d1711c09e42549407f5d8e
Forough Majidi <foroughmajidi1998@gmail.com>
2024-12-10 19:50:55 +0100

    feat: add welcome message to index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,50 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const userInfo = ref();
+const isLoading = ref(false);
+const isError = ref(false);
+
+async function loadUserInfo() {
+  try {
+    isError.value = false;
+    isLoading.value = true;
+    //TODO
+    userInfo.value = await fetch("").then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed to fetch user info");
+      }
+      return res.json();
+    });
+  } catch (error) {
+    isError.value = true;
+  } finally {
+    isLoading.value = false;
+  }
+}
+
+onMounted(() => {
+  loadUserInfo();
+});
+</script>
+
 <template>
-  <div></div>
+  <div class="p-4">
+    <!-- Error Message -->
+    <div v-if="isError" class="text-red-500">
+      An error occurred.
+      <button @click="loadUserInfo" class="text-blue-500 underline">
+        Retry
+      </button>
+    </div>
+
+    <div v-if="isLoading" class="text-gray-500">Loading user info...</div>
+
+    <!-- Welcome Message -->
+    <div v-if="!isLoading && !isError">
+      <h2 class="text-xl font-bold">
+        Welcome, {{ userInfo?.name || "Guest" }}!
+      </h2>
+    </div>
+  </div>
 </template>
